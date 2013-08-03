bf009621f9244770459401584ec0679b7cd3f31d
cao7113 <cao7113@hotmail.com>
2013-08-03 23:08:31 +0800

    add myapp

--- chef/roles/db.rb
+++ chef/roles/db.rb
@@ -1,3 +1,5 @@
 name "db"
 description "Database Server Role"
-run_list("recipe[mysql::server]")
+run_list("recipe[mysql::server]",
+         #"recipe[database::mysql]",
+         "recipe[myapp::db]")
