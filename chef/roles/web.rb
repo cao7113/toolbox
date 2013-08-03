a35c9c972576d241fd96f2e5f11cfd386d0b101c
cao7113 <cao7113@hotmail.com>
2013-08-03 21:42:43 +0800

    new

--- chef/roles/web.rb
+++ chef/roles/web.rb
@@ -1,6 +1,6 @@
 name "web"
 description "Web Server Role"
 run_list("recipe[apache2]",
-         "recipe[passenger_apache2::mod_rails",
+         "recipe[passenger_apache2::mod_rails]",
          "recipe[mysql::client]"
         )
